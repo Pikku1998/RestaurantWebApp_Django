a819fde96611dd256540fff95c109f0d85e61bd5
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-23 11:28:18 +0530

    Create a Database Model for Menu

--- restaurant_web_app/restaurant_menu/models.py
+++ restaurant_web_app/restaurant_menu/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ('appetizers', 'Appetizers'),
+    ('breads', 'Breads'),
+    ('soups_salads', 'Soups and Salads'),
+    ('chicken', 'Chicken specials'),
+    ('seafood', 'Seafood'),
+    ('biryani', 'Biryani'),
+    ('desserts', 'Desserts'),
+    ('beverages', 'Beverages')
+)
+
+STATUS = (
+    (0, 'Not Available'),
+    (1, 'Available')
+)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=300)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    meal_type = models.CharField(max_length=20, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
