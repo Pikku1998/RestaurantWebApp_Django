db99a9ae54a05ea49d24a2487e45f94f45d89aa3
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-23 15:13:39 +0530

    Render a simple index page with Class-based View.

--- restaurant_web_app/restaurant_menu/views.py
+++ restaurant_web_app/restaurant_menu/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Item, MEAL_TYPE
 
-# Create your views here.
+
+class MenuList(ListView):
+    queryset = Item.objects.all()
+    template_name = 'index.html'
+
+    def get_context_data(self):
+        context = {}
+        context['category'] = MEAL_TYPE
+        return context
+
